e4d128d49d3c70e97bff62d7c6ddf563a0827cea
sejal bhadawankar <129952007+PEAR2510@users.noreply.github.com>
2025-06-19 00:03:05 +0530

    Delete Rental.java

--- Rental.java
+++
@@ -1,9 +0,0 @@
-public class Rental {
-    private int rentalId;
-    private int carId;
-    private int customerId;
-    private Date rentalDate;
-    private Date returnDate;
-
-    // Constructor, getters, and setters
-}
