4c5afa4c7ca2c748d56d9fddd266e2ff05c9663d
sejal bhadawankar <129952007+PEAR2510@users.noreply.github.com>
2025-06-18 23:41:00 +0530

    Create Rental.java

---
+++ Rental.java
@@ -0,0 +1,9 @@
+public class Rental {
+    private int rentalId;
+    private int carId;
+    private int customerId;
+    private Date rentalDate;
+    private Date returnDate;
+
+    // Constructor, getters, and setters
+}
