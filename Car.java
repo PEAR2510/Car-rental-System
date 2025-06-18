dac3cbd3eef665492e55ba98cfa429a16dad3ade
sejal bhadawankar <129952007+PEAR2510@users.noreply.github.com>
2025-06-18 23:39:10 +0530

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    private int carId;
+    private String make;
+    private String model;
+    private int year;
+    private double pricePerDay;
+    private boolean availability;
+
+    // Constructor, getters, and setters
+}
